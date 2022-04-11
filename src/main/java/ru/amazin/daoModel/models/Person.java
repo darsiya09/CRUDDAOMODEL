54be0ca5d674b1167c1b37095037ee6a42affbdf
Aleksey Mazin <darsiya09@gmail.com>
2022-04-11 21:20:56 +0300

    Validator was added

--- src/main/java/ru/amazin/daoModel/models/Person.java
+++ src/main/java/ru/amazin/daoModel/models/Person.java
@@ -1,9 +1,24 @@
 package ru.amazin.daoModel.models;
 
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class Person {
+
     private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 2, max = 20, message = "Field name should contain from 2 to 20 characters")
     private String name;
+
+    @Min(value = 10, message = "Age should be greater then 0")
     private int age;
+
+    @NotEmpty(message = "Email should not be empty")
+    @Email(message = "Email should be valid")
     private String email;
 
     public Person() {
